7ee75698207d8bd0f4295f9e31f99696c112e9f2
nojas01 <j.r.nede@gmail.com>
2018-01-13 19:51:10 +0100

    get all data needed for a row insert

--- xpath.js
+++ xpath.js
@@ -1,10 +1,24 @@
 const fs        = require('fs');
 const XmlStream = require('xml-stream');
-/*
-   * Pass the ReadStream object to xml-stream
-*/
+const models = require('./models')
+
+
 const stream=fs.createReadStream('test.gpx');
 const xml = new XmlStream(stream);
+// xml.preserve('extensions', true)
+// xml.collect('subitem')
 xml.on('endElement: trkpt', function(row) {
-  console.log(row);
+  const newTraining = {}
+  newTraining.time = row.time
+  newTraining.latitude = row['$'].lat
+  newTraining.longitude = row['$'].lon
+  newTraining.cadance = row.extensions['ns3:TrackPointExtension']['ns3:cad']
+  newTraining.heartrate = row.extensions['ns3:TrackPointExtension']['ns3:hr']
+
+  console.log(newTraining);
+  models.training.create(newTraining)
+    .then((training) => {
+      res.json(training)
+    })
+    .catch((error) => console.log(error))
 });
