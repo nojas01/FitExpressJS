787c1f713c1e674c8b403d6915ce1f761494a337
nojas01 <j.r.nede@gmail.com>
2018-01-15 18:05:26 +0100

    routes for training fileupload. todo: assign foreignkey, fix callback errors due to loop

--- routes/trainings.js
+++ routes/trainings.js
@@ -4,13 +4,19 @@ const multer    = require('multer')
 const fs        = require('fs')
 const XmlStream = require('xml-stream')
 const router    = express.Router()
-const storage   = multer.memoryStorage()
-const upload    = multer({ storage: storage })
+const storage   = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '.gpx')
+    }
+  })
+const upload = multer({ storage: storage })
 
 router
-  .post('/trainings',, (req, res, next) => {
-      console.log(upload)
-      const stream = fs.createReadStream(upload)
+  .post('/trainings',upload.single('training'), (req, res, next) => {
+      const stream = fs.createReadStream('./uploads/training.gpx')
       const xml = new XmlStream(stream)
 
       xml.on('endElement: trkpt', function(row) {
@@ -21,9 +27,15 @@ router
         newTraining.cadance = row.extensions['ns3:TrackPointExtension']['ns3:cad']
         newTraining.heartrate = row.extensions['ns3:TrackPointExtension']['ns3:hr']
 
+        sequelize.transactions
         models.training.create(newTraining)
           .then((training) => { res.json(training) })
           .catch((error) => next(error))
+      })
+
+      fs.unlink('./uploads/training.gpx',(err) => {
+        if (err) throw err;
+        console.log('successfully deleted /tmp/hello');
       })
     })
 
