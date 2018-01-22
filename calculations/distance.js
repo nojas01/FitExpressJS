76c92c4eac5c0aadfe6bdf785e4a6a6ab35d427c
nojas01 <j.r.nede@gmail.com>
2018-01-22 17:46:51 +0100

    fixed calculation error. The problem was sorting

--- calculations/distance.js
+++ calculations/distance.js
@@ -5,7 +5,7 @@ const config    = require(__dirname + '/../config/config.json')[env];
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
 
-sequelize.query( "SELECT latitude, longitude FROM trainings", {type: Sequelize.QueryTypes.SELECT})
+sequelize.query( "SELECT latitude, longitude, time FROM trainings ORDER BY time", {type: Sequelize.QueryTypes.SELECT})
   .then((position) => {
     let totalDistance = 0
     for (let i = 1; i < position.length; i++) {
@@ -16,9 +16,24 @@ sequelize.query( "SELECT latitude, longitude FROM trainings", {type: Sequelize.Q
     	let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
     	dist = Math.acos(dist)
     	dist = dist * 180/Math.PI
-    	dist = dist * 60
+    	dist = dist * 60 * 1.1515 * 1.609344
       totalDistance += dist
     }
     console.log(totalDistance);
 })
 .catch((error) => console.log(error))
+
+//
+// function distance(lat1, lon1, lat2, lon2, unit) {
+// 	var radlat1 = Math.PI * lat1/180
+// 	var radlat2 = Math.PI * lat2/180
+// 	var theta = lon1-lon2
+// 	var radtheta = Math.PI * theta/180
+// 	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+// 	dist = Math.acos(dist)
+// 	dist = dist * 180/Math.PI
+// 	dist = dist * 60 * 1.1515
+// 	if (unit=="K") { dist = dist * 1.609344 }
+// 	if (unit=="N") { dist = dist * 0.8684 }
+// 	return dist
+// }
