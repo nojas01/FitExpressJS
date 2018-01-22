0b6c88a431026b49fd2d9019429203e8d4fa9600
nojas01 <j.r.nede@gmail.com>
2018-01-22 16:20:11 +0100

    get avarage cadance

---
+++ calculations/averageCadence.js
@@ -0,0 +1,27 @@
+const models = require('../models')
+const env       = process.env.NODE_ENV || 'development';
+const config    = require(__dirname + '/../config/config.json')[env];
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(config.database, config.username, config.password, config)
+
+
+sequelize.query( "SELECT avg(cadance) FROM trainings", { type: Sequelize.QueryTypes.SELECT})
+  .then((avg) => console.log(avg))
+  .catch((error) => console.log(error))
+
+
+
+//
+// models.training.count()
+//   .then(count => {console.log(count)})
+//   .catch((error) => console.log(error))
+//
+// models.training.sum('cadance').then(sum => {
+// console.log(sum)
+// }).catch((error) => console.log(error))
+//
+//
+// // const question = "SELECT * FROM `Rowers` WHERE UserId =" + UserId
+// // sequelize.query( question, { type: Sequelize.QueryTypes.SELECT})
+// //   .then((rowers) => res.json(rowers))
+// //   .catch((error) => next(error))
